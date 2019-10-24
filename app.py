fe0e26f1f3dec06ec85c8e829ee4b9102e08598e
Ubuntu <ubuntu@ip-172-31-92-37.ec2.internal>
2019-10-24 20:26:29 +0000

    added homepage

--- app.py
+++ app.py
@@ -2,24 +2,25 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 import pymongo
 import re
+from bson.objectid import ObjectId 
 
 
-# MONGO_URI = os.getenv('MONGO_URI')
-# DATABASE_NAME = "projectDB"
-# COLLECTION_NAME = "firstMDB"
+MONGO_URI = os.getenv('MONGO_URI')
+DATABASE_NAME = "projectDB"
+COLLECTION_NAME = "profile"
 
-# def get_connection():
-#     conn = pymongo.MongoClient(MONGO_URI)
-#     return conn
-
-# app = Flask(__name__)
+def get_connection():
+    conn = pymongo.MongoClient(MONGO_URI)
+    return conn
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+def index():
+    conn = get_connection()
+    profiles = conn[DATABASE_NAME][COLLECTION_NAME].find()
+    return render_template('index.template.html', profiles=profiles)
 
 
 if __name__ == '__main__':
